31933b0903084b07d30ba688f640ce7fe7627daf
Sagar888-J <sagarj5000@gmail.com>
2026-04-06 15:13:34 +0530

    Updated Login/Register

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Manage Tours')
+
+@section('content')
+<div class="container py-5">
+    <div class="page-header">
+        <h1>Tour Management</h1>
+        <p>Explore and organize your travel packages.</p>
+    </div>
+
+    <div class="tour-grid">
+        <p>No tours found. Start by creating one!</p>
+    </div>
+</div>
+@endsection
